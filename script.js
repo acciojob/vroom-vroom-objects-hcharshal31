1d952ceba0f32504f17b2ffc84ecd7a1d96ed2f7
Harshal Chavan <91681985+hcharshal31@users.noreply.github.com>
2025-01-29 16:24:34 +0530

    created a submission

--- script.js
+++ script.js
@@ -2,18 +2,21 @@
 function Car(make, model) {
 	this.make = make;
 	this.model = model;
-	getMakeModel(){
-		return this.model;
-	}
 }
+
+Car.prototype.getMakeModel = () => `${this.make} ${this.model}`;
 
 function SportsCar(make, model, topSpeed) {
 	Car.call(this, make, model);
 	this.topSpeed = topSpeed;
-	getTopSpeed(){
-		return this.topSpeed;
-	}
 }
+
+SportsCar.prototype = Object.create(Car.prototype);
+SportsCar.prototype.constructor = SportsCar;
+
+SportsCar.prototype.getTopSpeed = function() {
+	return this.topSpeed;
+};
 
 const car = new SportsCar("Ferrari", "Testarossa", 200);
 console.log(car.getMakeModel());
