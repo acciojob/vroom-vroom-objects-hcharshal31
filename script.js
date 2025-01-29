1a08e7df19cd5ef9125cd58a1b777c95b6d7b3d1
Harshal Chavan <91681985+hcharshal31@users.noreply.github.com>
2025-01-29 16:10:24 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,7 +1,23 @@
 // Complete the js code
-function Car(make, model) {}
+function Car(make, model) {
+	this.make = make;
+	this.model = model;
+	getMakeModel(){
+		return this.model;
+	}
+}
 
-function SportsCar(make, model, topSpeed) {}
+function SportsCar(make, model, topSpeed) {
+	Car.call(this, make, model);
+	this.topSpeed = topSpeed;
+	getTopSpeed(){
+		return this.topSpeed;
+	}
+}
+
+const car = new SportsCar("Ferrari", "Testarossa", 200);
+console.log(car.getMakeModel());
+console.log(car.getTopSpeed()); 
 
 // Do not change the code below
 window.Car = Car;
